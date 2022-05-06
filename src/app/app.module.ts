c1611c9f1c63aec34a0874be14f50a16e2223c61
Carolinaalecrim <crolina.glauseralecrim@gmail.com>
2022-05-06 20:42:33 -0300

    prjGeek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProdutoComponent } from './pages/produto/produto.component';
+import { CatalagoComponent } from './pages/catalago/catalago.component';
+import { AddProdutoComponent } from './pages/add-produto/add-produto.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    ProdutoComponent,
+    CatalagoComponent,
+    AddProdutoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
