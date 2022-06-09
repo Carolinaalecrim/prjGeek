f4098a1e66718bcff0d4a3e6217d685980ea8b14
Carolinaalecrim <carolina.glauseralecrim@gmail.com>
2022-06-08 22:29:07 -0300

    feito a integracao da pasta detalhes e produto

--- src/app/pages/produto/produto.component.ts
+++ src/app/pages/produto/produto.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Produto } from 'src/app/interface/produto';
 
 @Component({
   selector: 'app-produto',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoComponent implements OnInit {
 
-  constructor() { }
+  produto: Produto = {
+    idProduto: 0,
+    idCategoria: 0,
+    nome: '',
+    descricao: '',
+    preco: 0,
+    imagem: ''
+  };
+
+  produtos: Produto[] = [];
+
+  constructor(
+    private router: ActivatedRoute,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+    const url = "http://lucasreno.kinghost.net/loja/produto/";
+    const urlProdutos = "http://lucasreno.kinghost.net/loja/produtos";
+
+    this.http.get<any>(urlProdutos).subscribe(
+      resposta => {
+        this.produtos = resposta;
+      }
+    );
+    
+    this.router.params.subscribe(
+      params => {
+        this.http.get<any>(url + params["id"]).subscribe(
+          resposta => {
+            this.produto = resposta[0];
+          }
+        );
+      }
+    );
   }
 
 }
